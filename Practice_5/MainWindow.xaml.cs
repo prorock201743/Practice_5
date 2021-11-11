dae7bbbae2c895733320402ad5ac713d7dfa45b8
prorock201743 <zolotm24@gmail.com>
2021-11-11 18:39:50 +0300

    v1

---
+++ Practice_5/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+        private void Сalculate2Pair_Click(object sender, RoutedEventArgs e)
+        {
+            Pair firstPair = new Pair(int.Parse(firstP1.Text), int.Parse(secondP1.Text));
+            Pair secondPair = new Pair(int.Parse(firstP2.Text), int.Parse(secondP2.Text));
+            Pair resultPair = firstPair.Substract(secondPair);
+            result.Text = (resultPair.First, resultPair.Second).ToString();
+        }
+
+        private void Сalculate3Pair_Click(object sender, RoutedEventArgs e)
+        {
+            Pair firstPair = new Pair(int.Parse(firstP1.Text), int.Parse(secondP1.Text));
+            Pair secondPair = new Pair(int.Parse(firstP2.Text), int.Parse(secondP2.Text));
+            Pair thirdPair = new Pair(int.Parse(firstP3.Text), int.Parse(secondP3.Text));
+            Pair resultPair = firstPair.Substract(secondPair, thirdPair);
+            result.Text = (resultPair.First, resultPair.Second).ToString();
+        }
+
+        private void Multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            Pair firstPair = new Pair(int.Parse(firstP1.Text), int.Parse(secondP1.Text));
+            result.Text = firstPair.Multiply().ToString();
+        }
+
+        private void IsPeer_Click(object sender, RoutedEventArgs e)
+        {
+            Pair firstPair = new Pair(int.Parse(firstP1.Text), int.Parse(secondP1.Text));
+            result.Text = firstPair.IsEuqual().ToString();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Золотарев М. А.\n Группа: ИСП-34\n Вариант №13", "Разработчик", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ Practice_5/Pair.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_5
+{
+    class Pair
+    {
+        public int First { get; private set; }
+        public int Second { get; private set; }
+
+        public Pair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public bool IsEuqual() => First == Second;
+
+        public long Multiply() => First * Second;
+
+        public Pair Substract(Pair subtrahend) => new Pair(First - subtrahend.First, Second - subtrahend.Second);
+
+        public Pair Substract(Pair subtrahend1, Pair subtrahend2) => new Pair(First - subtrahend1.First - subtrahend2.First,
+                Second - subtrahend1.Second - subtrahend2.Second);
+    }
+}
